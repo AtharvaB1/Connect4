99830c165e94ceb1c3342d8db0f6775880116ecf
Atharva Bisht <k1910791@students.katyisd.org>
2025-09-04 08:34:26 -0500

    aw man

--- Connect4.py
+++ Connect4.py
@@ -7,7 +7,8 @@ root.title("Connect Four")
 count = 0
 clicked = True 
 buttons = []
-butCount = 0
+canvases = []
+chips = [[" " for i in range (7)] for j in range(6)]
 
 def reset():
     global clicked, count, buttons, butCount
@@ -15,13 +16,11 @@ def reset():
     clicked = True
     count = 0
     butCount = 0
-    for i in range(42):
-        buttons.append(Button(root,text=" ", font=("Times New Roman",12),height=4,width=8,bg="SystemButtonFace",command=lambda: b_click(buttons[i]))) 
-    for i in range(6):
-        for j in range(7):
-            z = buttons[butCount]
-            z.grid(row=i,column=j)
-            butCount+=1
+    for i in range(7):
+        buttons.append(Button(root,text="Drop Checker", font=("Times New Roman",12),height=5,width=10,border=3,highlightthickness=4,bg="SystemButtonFace",command=lambda: b_click(buttons[i])))  
+        canvases.append(Canvas(root,height=1000,width=120))
+        x = buttons[i]
+        x.grid(column=i,row=0)
     
 def checkWin(dir,count):
     pass
@@ -29,14 +28,22 @@ def checkWin(dir,count):
 def b_click(b):
     pass
 
-for i in range(42):
+for i in range(7):
     #buttons.append(Button(root,text=" ", font=("Times New Roman",12),height=4,width=8,bg="SystemButtonFace",command=lambda: b_click(buttons[i])))
-    buttons.append(Button(root,text=" ", font=("Times New Roman",12),height=4,width=8,border=3,highlightthickness=4,bg="SystemButtonFace",command=lambda: b_click(buttons[i])))  
-for i in range(6):
-    for j in range(7):
-        z = buttons[butCount]
-        z.grid(row=i,column=j,padx=10,pady=10)
-        butCount+=1
+    buttons.append(Button(root,text="Drop Checker", font=("Times New Roman",12),height=5,width=10,border=3,highlightthickness=4,bg="SystemButtonFace",command=lambda: b_click(buttons[i])))  
+    canvases.append(Canvas(root,height=670,width=100))
+    z = canvases[i]
+    x = buttons[i]
+    x.grid(column=i,row=0)
+    z.grid(column=i,row=1)
+
+    z.create_oval(2,10,100,110)
+    z.create_oval(2,120,100,220)
+    z.create_oval(2,230,100,330)
+    z.create_oval(2,340,100,440)
+    z.create_oval(2,450,100,550)
+    z.create_oval(2,560,100,660)
+    
 
 my_menu = Menu(root)
 root.config(menu=my_menu)
